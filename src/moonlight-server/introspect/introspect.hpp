87050bea4bb7632bd9243308aeb5b1bde7867294
ABeltramo <ABeltramo@users.noreply.github.com>
2025-09-16 18:30:29 +0000

    Committing clang-format changes

--- src/moonlight-server/introspect/introspect.hpp
+++ src/moonlight-server/introspect/introspect.hpp
@@ -12,6 +12,7 @@ std::optional<wolf::core::docker::Container> get_current_container(const wolf::c
 /**
  * Resolves the host path corresponding to a given local path (if externally mounted).
  */
-std::optional<std::string> get_host_path_for(const wolf::core::docker::Container &container, std::string_view local_path);
+std::optional<std::string> get_host_path_for(const wolf::core::docker::Container &container,
+                                             std::string_view local_path);
 
 } // namespace introspect
